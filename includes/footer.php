cc5464216361c2b9aa8cd79d6fb47c6df26676d3
ShubhraJyoti10 <120559001+ShubhraJyoti10@users.noreply.github.com>
2022-12-14 17:05:51 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,76 @@
+<!--find match Start-->
+<div class="find-match-d">
+    <div class="container">
+        <div class="row">
+            <div class="find-match-box">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <p class="fm-text1 Poppins-Semi-Bold f-23 color-d">Begin a blissful journey!</p>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="fm-btn1 Poppins-Semi-Bolf f-23 color-d">
+                        <a href="contact.php" class="color-f f-19 Poppins-Medium fm-btn2">Find Your Match Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Find match End-->
+
+
+<div class="container">
+    <div class="row">
+        <div class="footer-d">
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <p class="Poppins-Medium f-24 color-1b footer-c1">At Your Service</p>
+                <ul class="footer-ul">
+                    <!--<li><a href="login.html" class="Poppins-Regular f-15 color-1b footer-a">Member Login</a></li>-->
+                    <li><a href="register.php" class="Poppins-Regular f-15 color-1b footer-a">Sign Up</a></li>
+                    <!--<li><a href="#" class="Poppins-Regular f-15 color-1b footer-a">Ashram Wedding</a></li>-->
+                    <li><a href="faq.php" class="Poppins-Regular f-15 color-1b footer-a">How to use/FAQ</a></li>
+                    <li><a href="premium-member.php" class="Poppins-Regular f-15 color-1b footer-a">Premium Membership</a></li>
+                    <!--<li><a href="customer-support.html" class="Poppins-Regular f-15 color-1b footer-a">Customer support</a></li>-->
+                    <!--<li><a href="report-misuse.html" class="Poppins-Regular f-15 color-1b footer-a">Report Abuse</a></li>-->
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <p class="Poppins-Medium f-24 color-1b footer-c1">Our Organisation</p>
+                <ul class="footer-ul">
+                    <li><a href="about-us.php" class="Poppins-Regular f-15 color-1b footer-a">About Us</a></li>
+                    <!--<li><a href="insights.html" class="Poppins-Regular f-15 color-1b footer-a">Insights</a></li>-->
+                    <li><a href="contact.php" class="Poppins-Regular f-15 color-1b footer-a">Contact Us</a></li>
+                    <!--<li><a href="careers.html" class="Poppins-Regular f-15 color-1b footer-a">Careers</a></li>-->
+                    <li><a href="success-story.php" class="Poppins-Regular f-15 color-1b footer-a">Success Stories</a></li>
+                    <!--<li><a target="_blank" href="blog/index.html" class="Poppins-Regular f-15 color-1b footer-a">Blog</a></li>-->
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <p class="Poppins-Medium f-24 color-1b footer-c1">Your Advantage</p>
+                <ul class="footer-ul">
+
+                    <!--<li><a class="Poppins-Regular f-15 color-1b footer-a" href="#">Be safe online</a></li>-->
+
+                    <li><a class="Poppins-Regular f-15 color-1b footer-a" href="#">Disclaimer</a></li>
+
+                    <li><a class="Poppins-Regular f-15 color-1b footer-a" href="#">Privacy policy</a></li>
+
+                    <!--<li><a class="Poppins-Regular f-15 color-1b footer-a" href="#">Refund Policy</a></li>-->
+
+                    <li><a class="Poppins-Regular f-15 color-1b footer-a" href="#">Terms and Condition</a></li>
+                    <li><a href="#" class="Poppins-Regular f-15 color-1b footer-a">Sitemap</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <p class="Poppins-Medium f-24 color-1b footer-c1">Contact</p>
+                <ul class="footer-ul">
+                    <li><a href="javascript:void(0);" class="Poppins-Regular f-15 color-1b footer-a"><i class="fal fa-phone phone-i margin-icon"></i> +91-9999999999</a></li>
+                    <li><a href="javascript:void(0);" class="Poppins-Regular f-15 color-1b footer-a"><i class="far fa-clock clock-i margin-icon"></i> 9:30 AM to 6:30 PM only</a></li>
+                    <li><a href="mailto:info@shubhrajyoti.in" class="Poppins-Regular f-14 color-1b footer-a"><i class="far fa-envelope envelope-i margin-icon"></i> info@shubhrajyoti.in</a></li>
+                </ul>
+                <div class="col-xs-12 hidden-md hidden-lg hidden-sm padding-0">
+                    <a href="index.php"><img src="assets/logo/4fbfaf7bf3bee8df1f6c9f4727028457.jpg" alt="artofliving-logo" class="img-responsive footer logo"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,83 @@
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/fontawesome.min.css" integrity="sha384-z4tVnCr80ZcL0iufVdGQSUzNvJsKjEtqYZjiQrrYKlpGow+btDHDfQWkFjoaz/Zr" crossorigin="anonymous">
+
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+<div class="art-of-living-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4 col-xs-4 padding-l-r-0">
+                <a href="index.php">
+                    <div class="al-logo">
+                        <img src="assets/logo/4fbfaf7bf3bee8df1f6c9f4727028457.jpg" alt="Shubhra Jyoti Matrimony logo" class="img-responsive al-logo-img">
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-8 col-sm-8 col-xs-4 padding-l-r-0">
+                <div class="nav-icon">
+                    <!--                    <a href="https://play.google.com/store/apps/details?id=com.artofliving.matrimony" target="_blank">
+                                            <i class="fab fa-android android-icon"></i>
+                                        </a>
+                                        <a href="https://apps.apple.com/us/app/art-of-living-matrimony/id1491874764?ls=1" target="_blank">    
+                                            <i class="fab fa-apple apple-icon"></i>
+                                        </a>   -->
+                    <span class="btn-main">
+                        <!--                        <a href="login.html" class="al-reg-btn Poppins-Medium f-17 color-f">
+                                                    <span class="nav-loginbtn-cstm hidden-xs">Login</span>
+                                                </a>-->
+                        <a href="register.php" class="al-reg-btn Poppins-Medium f-17 color-f">
+                            <span class="nav-btn-cstm hidden-xs">Register </span>
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <!--mobile menu start-->
+            <div class="col-xs-4 col-sm-3 hidden-lg hidden-md hidden-sm">
+                <a href="javascript:void(0);" class="sidebar-icon rightMenuCall" id="sidebarCollapse"> <i class="fa fa-bars"></i> </a>
+            </div>
+            <div id="rightSideNav" class="rightLoggedSideBar" style="width: 0px;">
+                <div class="col-xs-12 col-sm-12 margin-top-0">
+                    <a href="javascript:void(0);" class="closeRightMenuBtn closebtn" onclick="">×</a>
+                    <!--<a href="login.html" class="btn btn-mega margin-top-10 margin-top-r-2">Login</a>-->
+                    <a href="register.php" class="btn btn-mega margin-top-r">Register </a>
+                </div>
+                <div class="col-xs-12 col-sm-12">
+                    <ul class="nav navbar-nav mt-11-position Poppins-Medium f-17 color-f nav-cstm">
+
+                        <li><a href="index.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-home home-icon"></i> Home</a></li>
+                        <li><a href="register.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-user user-icon"></i> Register</a></li>
+                        <li><a href="premium-member.php" class="Poppins-Medium f-17 color-f"><img src="assets/new_home/images/hexagon.png" alt="premium member">  Plans</a></li>
+                        <!--<li><a target="_blank" href="blog/index.html" class="Poppins-Medium f-17 color-f"><i class="fal fa-blog"></i>  Blog</a></li>-->
+                        <li><a href="success-story.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-restroom rest-r-icon"></i>  Success Stories</a> </li>
+                        <li><a href="event.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-calendar-plus calendar-icon"></i>  Events</a></li>			
+                        <li><a href="contact.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-phone phone-icon"></i>  Contact Us</a></li>                        </ul>
+                </div>
+            </div>
+            <!--mobile menu End-->
+        </div>
+    </div>
+</div>
+
+<div class="art-of-living-header-toggle hidden-xs">
+    <div class="container">
+        <div class="row">
+            <!--desktop menu start-->
+            <div class="col-md-12 col-xs-12 col-sm-12 hidden-xs padding-l-r-0">
+                <ul class="nav navbar-nav mt-11-position Poppins-Medium f-17 color-f nav-cstm">
+
+                    <li><a href="index.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-home home-icon"></i> Home</a></li>
+                    <li><a href="about-us.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-blog"></i>  About</a></li>
+                    <li><a href="event.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-calendar-plus calendar-icon"></i>  Events</a></li>
+
+<!--<li><a target="_blank" href="blog/index.html" class="Poppins-Medium f-17 color-f"><i class="fal fa-blog"></i>  Blog</a></li>-->
+
+                    <li><a href="success-story.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-restroom rest-r-icon"></i>  Success Stories</a> </li>
+                    <li><a href="premium-member.php" class="Poppins-Medium f-17 color-f"><img src="assets/new_home/images/hexagon.png" alt="premium member">  Plans</a></li>	
+                    <li><a href="contact.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-phone phone-icon"></i>  Contact Us</a></li>
+                    <!--<li><a href="register.php" class="Poppins-Medium f-17 color-f"><i class="fal fa-user user-icon"></i> Register</a></li>-->
+                </ul>
+            </div>
+            <!--desktop menu End-->
+        </div>
+    </div>
+</div>
