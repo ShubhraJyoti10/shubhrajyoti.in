cd5e4aa59e2689fa4dabc52b258fb945e2300836
shubhra jyoti <shubhraaniljyoti68@gmail.com>
2022-12-14 15:10:55 +0530

    Initial commit

---
+++ includes/copyright.php
@@ -0,0 +1,20 @@
+<div class="copyright-d">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <p class="Poppins-Regular f-15 color-f copyright-c1">All Rights Reserved by @ shubhrajyoti.in</p>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="al-social-icon">
+                    <span>
+                        <a href="#" target="_blank"><i class="fab fa-facebook-square color-f f-28 social-icon-common"></i></a>
+                        <a href="#" target="_blank"><i class="fab fa-twitter color-f f-28 social-icon-common"></i></a>
+                       <!-- <a href="" target="_blank"><i class="fab fa-google color-f f-28 social-icon-common"></i></a>-->
+                        <a href="#" target="_blank"><i class="fab fa-instagram color-f f-28 social-icon-common"></i></a>
+                        <a href="#" target="_blank"><i class="fab fa-pinterest-square color-f f-28 social-icon-common"></i></a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
